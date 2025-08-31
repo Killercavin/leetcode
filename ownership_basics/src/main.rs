a174eb2f2713256d8cb442b0354fe60e86dd88aa
Cavin <cavinlarry001@gmail.com>
2025-08-31 14:15:24 +0300

    Remove unused Rust project files and directories

--- ownership_basics/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let mut s = String::from("Hello");
-    s.push_str(", world!");
-    println!("{}", s);
-
-    // ownership and functions
-}
--- ownershp/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let s1 = String::from("Hello");
-    let s2 = s1; //.clone();
-    println!("{}, world!", s1);
-    // println!("Is s1 equal to s2? {}", s1 == s2); // This will not compile when clone is not used
-}
